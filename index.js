4cb5d92755d0bec0dcff48bbdfc756011d923aa4
Hemanathan <hphemanathan@gmail.com>
2024-01-07 17:41:37 +0530

    wip

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
 const showButton = document.querySelectorAll(".qAndA--question--icon__show");
 const hideButton = document.querySelectorAll(".qAndA--question--icon__hide");
+const showQuestion = document.querySelectorAll(".qAndA--question");
 
-console.log(hideButton);
 
-function handleShowAnswer () {
-hideButton.classList.toggle('hide');
+
+function handleShowAnswer(event) {
+   event.currentTarget.firstElementChild.nextElementSibling.classList.toggle("hide");
+    event.currentTarget.lastElementChild.classList.toggle('hide');
+    event.currentTarget.nextElementSibling.classList.toggle('show');
 }
 
-function handleButtonClick (button) {
-    button.addEventListener('click', handleShowAnswer)
+function showAnswer (button) {
+    button.addEventListener('click', handleShowAnswer);
 }
 
-showButton.forEach(handleButtonClick);
+
+showQuestion.forEach(showAnswer);
